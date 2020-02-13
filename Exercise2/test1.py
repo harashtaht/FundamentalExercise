390a4fce3ab3761cd2bd96c4e0c61246cf4cc627
Harashta <52948174+harashtaht@users.noreply.github.com>
2020-02-13 23:40:20 +0700

    Add files via upload

---
+++ Exercise2/test1.py
@@ -0,0 +1,36 @@
+# No 4 - Python
+
+def divisible(dictNum):
+    results = 0
+    cond1 = (1<=dictNum["A"]<=dictNum["B"]<10000)
+    cond2 = (1<=dictNum["K"]<10000)
+    if cond1 == True and cond2 == True:
+        for i in range(dictNum["A"], dictNum["B"]+1):
+            if i%dictNum["K"] == 0:
+                results+=1
+            else:
+                results+=0
+        return results
+    else:
+        return 0
+
+
+test_A = input()
+num_T = int(test_A)
+
+cond_init = (1<=num_T<=100)
+
+if cond_init==True:
+    dictOutput = {}
+    for i in range(1, num_T+1):
+        dictNum = {}
+        dictNum["A"] = int(input())
+        dictNum["B"] = int(input())
+        dictNum["K"] = int(input())
+        dictOutput[i] = divisible(dictNum)
+else:
+    breakpoint
+
+for i in range(1, len(dictOutput)+1):
+    print(f'Case {i}: {dictOutput[i]}')
+
