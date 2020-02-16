a28cb899676bca4defac9f5bdd1ba7e8a0329ed3
HHT <harashtatandjung@gmail.com>
2020-02-16 15:28:37 +0700

    ren

--- Exercise2/goj4.py
+++ Exercise2/goj4.py
@@ -1,67 +1,36 @@
-# A = int(input("A : "))
-# B = int(input("B : "))
-# K = int(input("K : "))
+# No 4 - Python
 
-# cond1 = (1<=A<=B<10000)
-# cond2 = (1<=K<10000)
-
-# if cond1 == True and cond2 == True:
-#     results = 0
-#     for i in range(A, B+1, 1):
-#         if i%K == 0:
-#             results +=1
-#             print(i, results)
-#         else:
-#             continue
-
-
-# print(3%3)
-# print(6%3)
-# print(9%3)
-
-# A : 281
-# B : 9155
-# K : 91
-
-# 364 1
-# 455 2
-# 546 3
-# 637 4
-# 728 5
-# 819 6
-# 910 7
-# 1001 8
-
-# print(364%91)
-# print(455%91)
-# print(546%91)
-# print(637%91)
-# print(728%91)
-# print(819%91)
-# print(910%91)
-# print(1001%91)
-
-# 6734 71
-# 6825 72
-# 6916 73
-# 7007 74
-# 7098 75
-# 7189 76
-# 7280 77
-# 7371 78
-# 7462 79
-# 7553 80
+def divisible(dictNum):
+    results = 0
+    cond1 = (1<=dictNum["A"]<=dictNum["B"]<10000)
+    cond2 = (1<=dictNum["K"]<10000)
+    if cond1 == True and cond2 == True:
+        for i in range(dictNum["A"], dictNum["B"]+1):
+            if i%dictNum["K"] == 0:
+                results+=1
+            else:
+                results+=0
+        return results
+    else:
+        return 0
 
 
-print(6734%91)
-print(6735%91)
-print(6736%91)
-print(6825%91)
-print(6916%91)
-print(7007%91)
-print(7098%91)
-print(7189%91)
-print(7280%91)
-print(7371%91)
-print(7462%91)
-print(7553%91)
+test_A = input()
+num_T = int(test_A)
+
+cond_init = (1<=num_T<=100)
+
+if cond_init==True:
+    dictOutput = {}
+    for i in range(1, num_T+1):
+        dictNum = {}
+        dictNum["A"] = int(input())
+        dictNum["B"] = int(input())
+        dictNum["K"] = int(input())
+        dictOutput[i] = divisible(dictNum)
+else:
+    breakpoint
+
+for i in range(1, len(dictOutput)+1):
+    print(f'Case {i}: {dictOutput[i]}')
+
---
+++ Exercise2/test4.py
@@ -0,0 +1,6 @@
+T = int(input())
+# print(T)
+# print(type(T))
+
+if 1<=T<=100:
+    
