dfbba792536f826db0ea2cf1d652bfb7d5501485
HHT <harashtatandjung@gmail.com>
2020-03-16 21:44:40 +0700

    agate3

--- agate2.sql
+++ agate2.sql
@@ -6,13 +6,14 @@ show tables;
 
 CREATE TABLE authors
 (
-	author_name VARCHAR(255) NOT NULL PRIMARY KEY,
+	author_name VARCHAR(255) NOT NULL,
     book_name VARCHAR(255) NOT NULL
     );
     
+    
 CREATE TABLE books
 (
-	book_name VARCHAR(255) NOT NULL PRIMARY KEY,
+	book_name VARCHAR(255) NOT NULL,
     sold_copies INT);
     
 INSERT INTO authors (author_name, book_name) VALUES
@@ -23,8 +24,25 @@ INSERT INTO authors (author_name, book_name) VALUES
 ("author_2", "book_5"),
 ("author_3", "book_6");
 
-SELECT * FROM authors;
+SELECT * FROM books;
+
+INSERT INTO books (sold_copies, book_name) VALUES
+(1000, "book_1"),
+(1500, "book_2"),
+(34000, "book_3"),
+(29000, "book_4"),
+(40000, "book_5"),
+(4400, "book_6");
 
 
-INSERT INTO ku_product (name, effective_date, effective_until, photo, price, status) VALUES
-("Basic Lunch",DATE("2019-12-22"),DATE("2019-12-28"),"basic-lunch.jpg",20000.0,2),
+SELECT author_name, sold_copies FROM authors a
+LEFT JOIN books b on a.book_name = b.book_name;
+
+## SUM sold books based on author, order by minimum
+
+
+-- SELECT u.id, u.name, u.email, u.phone FROM ku_user u
+-- RIGHT JOIN ku_order o on o.user_id = u.id
+-- WHERE o.status = 2;
+
+
