b07265760628f96865a6e78b4258b4c48e2fd0ab
HHT <harashtatandjung@gmail.com>
2020-03-17 15:28:44 +0700

    SQL4a

--- agate2.sql
+++ agate2.sql
@@ -102,4 +102,88 @@ FROM
 
 
 ######################
-#### 3. 
+#### 3. Print every department where the average salary per employee is over than 500$
+
+#JOIN by employee_id, GROUP by department_name
+#WHERE avg(salary) >500
+
+CREATE TABLE employees
+(
+	department_name VARCHAR(30),
+    employee_id INT NOT NULL PRIMARY KEY,
+    employee_name VARCHAR(255)
+    );
+    
+select * from employees;
+
+CREATE TABLE salaries
+(
+	salary INT,
+    employee_id INT NOT NULL PRIMARY KEY,
+    employee_name VARCHAR(255)
+    );
+    
+select * from salaries;
+
+INSERT INTO employees (department_name, employee_id, employee_name) 
+VALUES
+("Sales", 123, "John Doe"),
+("Sales", 211, "Jane Smith"),
+("HR", 556, "Billy Bob"),
+("Sales", 711, "Robert Hayek"),
+("Marketing", 235, "Edward Jorgson"),
+("Marketing", 236, "Christine Packard");
+
+INSERT INTO salaries (salary, employee_id, employee_name)
+values
+(500, 123, "John Doe"),
+(600, 211, "Jane Smith"),
+(1000, 556, "Billy Bob"),
+(400, 711, "Robert Hayek"),
+(1200, 235, "Edward Jorgson"),
+(200, 236, "Christine Packard");
+
+select department_name, avg(salary) from salaries s
+LEFT JOIN employees e on s.employee_id = e.employee_id
+GROUP BY department_name;
+
+select department_name, salary from salaries s
+LEFT JOIN employees e on s.employee_id = e.employee_id;
+
+## Display department_name with average_salary
+
+SELECT 
+    department_name,
+    CAST(AVG(salary) AS DECIMAL (10 , 2 )) AS average_sal
+FROM
+    employees e
+        LEFT JOIN
+    salaries s ON e.employee_id = s.employee_id
+GROUP BY department_name
+HAVING (average_sal > 500)
+ORDER BY average_sal DESC;
+
+
+### Answer no 3:
+## Display department_name only
+SELECT 
+    department_name
+FROM
+    (SELECT 
+        department_name,
+            CAST(AVG(salary) AS DECIMAL (10 , 2 )) AS average_sal
+    FROM
+        employees e
+    LEFT JOIN salaries s ON e.employee_id = s.employee_id
+    GROUP BY department_name
+    HAVING (average_sal > 500)) AS T
+    ORDER BY average_sal;
+
+### Real condition: (average_sal >500)
+
+
+######################
+#### 4.	Create SQL Query that show Person Data with each their Deposito Amount. 
+#### 	Data sorted by PERSON_ID.
+
+
