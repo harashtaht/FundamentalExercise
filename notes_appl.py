141fe9876325a9195abbad62ca6d1111174dc6d6
HHT <harashtatandjung@gmail.com>
2020-06-18 19:57:21 +0700

    dev.to app

--- notes_appl.py
+++ notes_appl.py
@@ -1,11 +1,11 @@
 day_count = 0
 
-present= int(input("Attendance? (0/1)"))
+present= int(input("Attendance? (0/1):"))
 rate = 800
 hour = int(input("Working hours:"))
 
 salary = 0
-while day_count<=20:
+while day_count<20:
     day_count +=1
     if present == 1:
         salary += rate*hour
