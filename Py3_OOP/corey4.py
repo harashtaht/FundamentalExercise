1f73fb4f2ae8935a6c407a2f0dcc385be62a93a7
HHT <harashtatandjung@gmail.com>
2020-06-19 18:55:34 +0700

    subclass1

--- Py3_OOP/corey4.py
+++ Py3_OOP/corey4.py
@@ -15,14 +15,47 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
 
+#inherit from Employee
 class Developer(Employee):
-    pass
+    raise_amt = 1.1
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+        
+        # Employee.__init__(self, first, last, pay) 
+        # also works but less maintainable
+
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname)
 
 
-dev_1 = Developer('Corey', 'Schafer', 50000)
-dev_2 = Developer('Test', 'Employee', 60000)
+dev_1 = Developer('Corey', 'Schafer', 50000, 'Python')
+dev_2 = Developer('Test', 'Employee', 60000, 'Java')
 
-# print(dev_1.email)
-# print(dev_2.email)
+print(dev_1.email)
+print(dev_1.prog_lang)
 
-print(help(Developer))
+
+# print(dev_1.pay)
+# dev_1.apply_raise()
+# print(dev_1.pay)
+
+# print(help(Developer))
