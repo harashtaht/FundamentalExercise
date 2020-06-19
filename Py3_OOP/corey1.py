90d75aae3cc97fc9c89b7049972f99e14c9654d0
HHT <harashtatandjung@gmail.com>
2020-06-19 12:39:46 +0700

    1

--- Py3_OOP/corey1.py
+++ Py3_OOP/corey1.py
@@ -2,24 +2,49 @@
 
 # 1 - Initial
 
-class Employee:
-    pass
+# class Employee:
+#     pass
 
-emp_1 = Employee()
-emp_2 = Employee()
+# emp_1 = Employee()
+# emp_2 = Employee()
+
+# # print(emp_1)
+# # print(emp_2)
+
+# emp_1.first = 'Corey'
+# emp_1.last = 'Schafer'
+# emp_1.email = 'Corey.Schafer@company.com'
+# emp_1.pay = 50000
+
+# emp_2.first = 'Test'
+# emp_2.last = 'User'
+# emp_2.email = 'Test.User@company.com'
+# emp_2.pay = 60000
+
+# print(emp_1.email)
+# print(emp_2.email)
+
+
+# 2 - class instance
+
+class Employee:
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        # print(self.first + " " + self.last)
+        return '{} {}'.format(self.first, self.last)
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'User', 60000)
 
 # print(emp_1)
-# print(emp_2)
+# print(emp_1.email)
+# print(emp_2.email)
 
-emp_1.first = 'Corey'
-emp_1.last = 'Schafer'
-emp_1.email = 'Corey.Schafer@company.com'
-emp_1.pay = 50000
-
-emp_2.first = 'Test'
-emp_2.last = 'User'
-emp_2.email = 'Test.User@company.com'
-emp_2.pay = 60000
-
-print(emp_1.email)
-print(emp_2.email)
+print(emp_1.fullname())
+print(Employee.fullname(emp_1))
