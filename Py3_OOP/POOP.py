843225f3ce1e60e8eb1a56e9310a516cc61c53ca
HHT <harashtatandjung@gmail.com>
2020-06-19 09:46:54 +0700

    Class

--- Py3_OOP/POOP.py
+++ Py3_OOP/POOP.py
@@ -1,4 +1,5 @@
 # Object Orientated Programming in Python
+# Part 1
 
 def hello():
     print("hello")
@@ -58,6 +59,35 @@ class Dog:
 d = Dog("Tim", 3)
 d2 = Dog("Bill", 6)
 
-print(d.get_name())
-d.set_age(14)
-print(d.get_age())
+# print(d.get_name())
+# d.set_age(14)
+# print(d.get_age())
+
+# Part 2
+
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade # 0 - 100
+
+    def get_grade(self):
+        return self.grade
+
+class Course:
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []
+        # self.is_active = False
+    
+    def add_student(self, student):
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+            return True
+        return False
+
+    def get_average_grade(self):
+        pass
+
+
