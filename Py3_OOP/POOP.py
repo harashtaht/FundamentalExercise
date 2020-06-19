c8aa3b5cc02f1d31ddb4fdb0fc4c506051d78261
HHT <harashtatandjung@gmail.com>
2020-06-19 10:45:32 +0700

    p4

--- Py3_OOP/POOP.py
+++ Py3_OOP/POOP.py
@@ -175,4 +175,18 @@ class Fish(Pet):
 # f.show()
 # f.speak()
 
-## -- Part 4 : -- ##
+## -- Part 4 : Class Attributes -- ##
+
+# class Person:
+#     number_of_people = 0
+
+#     def __init__(self, name):
+#         self.name = name
+#         Person.number_of_people += 1
+
+# p1 = Person("tim")
+# print(Person.number_of_people)
+# p2 = Person("jill")
+# # Person.number_of_people = 8
+# print(Person.number_of_people)
+
