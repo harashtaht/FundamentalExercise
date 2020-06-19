b4997364ff2187b51063be98f9ac1fc7780e644e
HHT <harashtatandjung@gmail.com>
2020-06-19 10:06:13 +0700

    inh

--- Py3_OOP/POOP.py
+++ Py3_OOP/POOP.py
@@ -109,3 +109,70 @@ course.add_student(s2)
 
 ## -- Part 3 : Inheritance -- ##
 
+# INIT 1
+# class Cat:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def speak(self):
+#         print("Meow")
+
+# class Dog:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def speak(self):
+#         print("Bark")
+
+# Modified for inheritance
+
+# Upper level class
+class Pet:
+    def __init__ (self, name, age):
+        self.name = name
+        self.age = age
+
+    def show(self):
+        print(f"I am {self.name} and I am {self.age} years old")
+
+    def speak(self):
+        print("I don't know what to say")
+
+# Child classes
+class Cat(Pet):
+    def __init__(self, name, age, color):
+        super().__init__(name, age)
+        self.color = color
+
+    def speak(self):
+        print("Meow")
+
+    def show(self):
+        print(f"I am {self.name} and I am {self.age} years old and I am {self.color}")
+
+class Dog(Pet):
+    def speak(self):
+        print("Bark")
+
+class Fish(Pet):
+    pass
+
+# p = Pet("Tim", 19)
+# p.show()
+# p.speak()
+
+# c = Cat("Bill", 34, "brown")
+# c.show()
+# c.speak()
+
+# d = Dog("Jill", 25)
+# d.show()
+# d.speak()
+
+# f = Fish("Bubbles", 10)
+# f.show()
+# f.speak()
+
+## -- Part 4 : -- ##
