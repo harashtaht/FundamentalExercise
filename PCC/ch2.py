3b54a8193a81ef974b4c69c2a99960760fbd4f63
HHT <harashtatandjung@gmail.com>
2020-06-22 07:03:07 +0700

    var notes

--- PCC/ch2.py
+++ PCC/ch2.py
@@ -2,3 +2,19 @@
 
 message = "Hello Python world!"
 print(message)
+
+
+message = "Hello Python Crash Course World!"
+print(message)
+
+'''
+we added a variable named message, and the string is the value.
+few variables rules:
+- v. names can only contain letters, numbers and underscores. 
+    can not start with a number.
+- spaces are not allowed
+- avoid using Python keywords and function names as v.names.
+- should be short but descriptive
+- be careful when using the lowercase letter l and uppercase letter O, 
+    they could be confused w/ numbers 1 and 0
+'''
