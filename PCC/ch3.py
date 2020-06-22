0b90a76e66924d620889b989b9df604d5b21f765
HHT <harashtatandjung@gmail.com>
2020-06-22 08:48:42 +0700

    list

---
+++ PCC/ch3.py
@@ -0,0 +1,18 @@
+# -- Chapter 3 : Introducing Lists -- #
+
+'''
+A list is a collection of items in a particular order.
+You can make a list that includes the letters of the alphabet, 
+the digits from 0-9, or the names of all the people in your family.
+'''
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+# print(bicycles[2].title())
+# print(bicycles[-1].title())
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
