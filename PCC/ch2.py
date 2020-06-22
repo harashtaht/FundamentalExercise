44c9e118e93bf95cf16851ed8a6b65aed4e13b5a
HHT <harashtatandjung@gmail.com>
2020-06-22 08:22:39 +0700

    ZOP

--- PCC/ch2.py
+++ PCC/ch2.py
@@ -71,3 +71,14 @@ x = 16.0/7
 
 'Documentary to check - https://docs.python.org/3/tutorial/index.html'
 
+'''
+The Zen of Python, by Tim Peters.
+
+Beautiful is better than ugly.
+Simple is better than complex.
+Complex is better than complicated.
+Readability counts.
+There should be one-- and preferably only one --obvious way to do it.
+Now is better than never.
+'''
+
