1a93dfe543d48a3f1dceecf1b9293d8b79b2777d
HHT <harashtatandjung@gmail.com>
2020-06-24 19:00:28 +0700

    p79

--- PCC/ch5.py
+++ PCC/ch5.py
@@ -2,8 +2,26 @@
 
 cars = ['audi', 'bmw', 'subaru', 'toyota']
 
-for car in cars:
-    if car == 'bmw':
-        print(car.upper())
-    else:
-        print(car.title())
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+
+# a - Conditional Tests
+
+'''
+At the heart of every if statement is an expression that can be evaluated as True or False
+and is called a conditional test.
+Testing for equality is case sensitive in Python.
+
+'''
+
+# car = 'Audi'
+# print(car.lower() == 'audi') #True
+# print(car == 'audi') #False
+
+requested_topping = 'mushrooms'
+
+if requested_topping != 'anchovies':
+    print("Hold the anchovies")
