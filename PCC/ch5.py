be146a03e86916c062e03ca797c3e3f102bbc01a
HHT <harashtatandjung@gmail.com>
2020-06-24 19:10:14 +0700

    82

--- PCC/ch5.py
+++ PCC/ch5.py
@@ -21,7 +21,25 @@ Testing for equality is case sensitive in Python.
 # print(car.lower() == 'audi') #True
 # print(car == 'audi') #False
 
-requested_topping = 'mushrooms'
+# requested_topping = 'mushrooms'
 
-if requested_topping != 'anchovies':
-    print("Hold the anchovies")
+# if requested_topping != 'anchovies':
+#     print("Hold the anchovies")
+
+age_0 = 22
+age_1 = 18
+
+# print((age_0>=21) and (age_1<=21))
+
+requested_topping = ['mushrooms', 'onions', 'pineapple']
+
+# print('mushrooms' in requested_topping)
+
+banned_users = ['andrew', 'carolina', 'david']
+user = ['marie', 'andrew']
+
+for i in user:
+    if i not in banned_users:
+        print(i.title()+", you can post a response if you wish")
+    else:
+        print("shut the fuck up, " + i.title() + ". You're motherfucking banned, you bitch.")
