6656206215457c44d4505d938e5ca6f9925eb952
HHT <harashtatandjung@gmail.com>
2020-06-28 12:24:28 +0700

    147

--- PCC/ch8.py
+++ PCC/ch8.py
@@ -40,3 +40,77 @@ A keyword argument is a name-value pair that you pass to a function.
 
 # describe_pet(animal_type='horse', pet_name='butcher')
 # describe_pet(pet_name='johnnyboy', animal_type='penguin')
+
+## Default Values ##
+'''
+When writing a function, you can define a default value for each parameter.
+If an argument for the parameter is not provided in function call, Python uses the 
+default value.
+'''
+
+
+def describe_pet(pet_name, animal_type='dog'):
+    "Display information about a pet."
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+# describe_pet('harry')
+
+# describe_pet('johnny', animal_type='Labrador')
+
+
+## Return Values ##
+
+def get_formatted(first_name, last_name, middle_name=''):
+    "Return a full name, neatly formatted."
+    if middle_name:
+        full_name = first_name + ' ' + middle_name + ' '+ last_name   
+    else:
+        full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+musician = get_formatted('jimi', 'hendrix')
+# print(musician)
+
+musician = get_formatted('jimi', 'hendrix', 'lee')
+# print(musician)
+
+#
+# Make a nested function, for first_name last_name using while.
+#
+
+
+def build_person(first_name, last_name, age=''):
+    person = {'first': first_name, 'last':last_name}
+    if age:
+        person['age'] = age
+    return person
+
+# print(build_person('jimi', 'hendrix', age=27))
+
+
+def get_formatted_name(first_name, last_name):
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+# while True:
+#     print("\nPlease tell me your name:")
+#     print("(enter 'q' at any time to quit)")
+
+#     f_name = input("First name: ")
+#     if f_name == 'q':
+#         break
+
+#     l_name = input("Last name: ")
+#     if l_name == 'q':
+#         break
+
+#     print("Hello, ", get_formatted_name(f_name, l_name))
+
+def greet_user(names):
+    for name in names:
+        msg = "Hello, " + name.title() + "!"
+        print(msg)
+
+usernames = ['hannah', 'maria', 'jesus']
+greet_user(usernames)
