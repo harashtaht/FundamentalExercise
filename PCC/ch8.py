b6116b409bb7235d2697fe0c8bdd543cabd22fc0
HHT <harashtatandjung@gmail.com>
2020-06-28 15:06:01 +0700

    8init

--- PCC/ch8.py
+++ PCC/ch8.py
@@ -112,5 +112,34 @@ def greet_user(names):
         msg = "Hello, " + name.title() + "!"
         print(msg)
 
-usernames = ['hannah', 'maria', 'jesus']
-greet_user(usernames)
+# usernames = ['hannah', 'maria', 'jesus']
+# greet_user(usernames)
+
+
+def print_models(unprinted_designs, completed_models):
+    '''
+    Simulate printing each designs, until none are left. 
+    The unprinted designs will be duplicated, so the original still available to access.
+    Move each designs to completed_models after printing.
+    '''
+
+    copy_unprinted = unprinted_designs[:]
+    while copy_unprinted:
+        current_design = copy_unprinted.pop()
+
+        print("Printing model: "+ current_design)
+        completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+    
+    '''Show all the models that were printed.'''
+
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
+unprinted_designs = ['iphone case', 'robot pendadnt', 'dodecahedron']
+completed_models = []
+
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
