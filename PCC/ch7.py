942aa4eb5ca633b558a5d2cc47c479d1679ec1b7
HHT <harashtatandjung@gmail.com>
2020-06-28 09:46:27 +0700

    ch7 - 121

---
+++ PCC/ch7.py
@@ -0,0 +1,27 @@
+# -- Chapter 7 : User Input and While Loops -- #
+
+'''
+The input() function pauses your program and waits for the user to enter some text.
+Once Python receives the user's input,
+it stores it in a variable to make it convenient for you to work with.
+The input() function takes one argument: the prompt, or instructions,
+that we want to display to the user so they know what to do.
+'''
+
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+
+# name = input(prompt)
+# print("\nHello, " + name + "!")
+
+'''
+When you use the input() function, Python interprets everything the user enters as a string.
+'''
+
+# height = input("How tall are you, in inches? ")
+# height = int(height)
+
+# if height >= 36:
+#     print("\nYou're tall enough to ride!")
+# else:
+#     print("\nYou'll be able to ride when you're a little older.")
