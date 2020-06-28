26737b7ce305aa2f4b44353f264782d460bab1ed
HHT <harashtatandjung@gmail.com>
2020-06-28 10:29:05 +0700

    8 init

--- PCC/ch7.py
+++ PCC/ch7.py
@@ -77,11 +77,55 @@ you can use the continue statement to return to the beginning of the loop based 
 the result of a conditional test.
 """
 
-current_number = 0
-while current_number < 10:
-    current_number += 1
-    if current_number %2 == 0:
-        continue
+# current_number = 0
+# while current_number < 10:
+#     current_number += 1
+#     if current_number %2 == 0:
+#         continue
 
-    print(current_number)
+#     print(current_number)
 
+
+# Start with users that need to be verified,
+# and an empty list to hold confirmed users.
+
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+
+# Verify each user until there are no more unconfirmed users.
+#  Move each verified user into the list of confirmed users.
+
+# while unconfirmed_users:
+#     current_user = unconfirmed_users.pop()
+
+#     print("Verifying users: " + current_user.title())
+#     confirmed_users.append(current_user)
+
+#     # Display all confirmed users.
+#     print("\nThe following users have been confirmed:")
+#     for confirmed_user in confirmed_users:
+#         print(confirmed_user.title())
+
+
+responses = {}
+
+# Set a flag to indicate that polling is active.
+polling_active = True
+
+while polling_active:
+    # Prompt for the person's name and response.
+    name = input("\nWhat is your name? ")
+    response = input("Which mountain would you like to climb someday? ")
+
+    # Store the response in the dictionary
+    responses[name] = response
+
+    # Find out if anyone else is going to take the poll.
+    repeat = input("Would you like to let another person respond? (yes/no) ")
+    if repeat == 'no':
+        polling_active = False
+
+# Polling is complete. Showing the results.
+print('\n-- Poll Results --')
+for name, response in responses.items():
+    print(name + " would like to climb " + response + ".")
