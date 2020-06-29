ca501e7c7b6d2e91f2056e825cfdfb72077081c8
HHT <harashtatandjung@gmail.com>
2020-06-30 06:37:39 +0700

    168

--- PCC/ch9.py
+++ PCC/ch9.py
@@ -33,3 +33,29 @@ whenever we create a new instance based on the Dog class.
 Variables that are accessible through instances like this are called attributes. 
 -> i.e. self.age, self.name
 '''
+
+my_dog = Dog('willie', 6)
+
+# print(my_dog.name.title())
+# print(my_dog.age)
+# my_dog.sit()
+
+
+class Car():
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = f"{str(self.year)} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        print(f"This car has {str(self.odometer_reading)} miles on it.")
+
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
