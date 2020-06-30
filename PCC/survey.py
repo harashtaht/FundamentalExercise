febbce94d4cee9b1be9f0976a15124570e8e281b
HHT <harashtatandjung@gmail.com>
2020-06-30 16:12:17 +0700

    Project__init__

--- PCC/survey.py
+++ PCC/survey.py
@@ -6,16 +6,16 @@ class AnonymousSurvey():
         self.question = question
         self.responses = []
 
-    def show_question(self):
-        # Show the survey question.
-        print(question)
+        def show_question(self):
+            # Show the survey question.
+            print(question)
 
-    def store_response(self, new_response):
-        # Store a single response to the survey.
-        self.responses.append(new_response)
+        def store_response(self, new_response):
+            # Store a single response to the survey.
+            self.responses.append(new_response)
 
-    def show_results(self):
-        # Show all the responses that have been given.
-        print("Survey results: ")
-        for response in responses:
-            print('-  ' + response)
+        def show_results(self):
+            # Show all the responses that have been given.
+            print("Survey results: ")
+            for response in self.responses:
+                print('-  ' + response)
