7f067a822494ab616acf1b8045c8343f912e0566
HHT <harashtatandjung@gmail.com>
2020-07-07 20:04:39 +0700

    update game_functions.py

--- PCC/Part_2_Projects/game_functions.py
+++ PCC/Part_2_Projects/game_functions.py
@@ -1,6 +1,20 @@
 import sys
 import pygame
 
+def check_keydown_events(event, ship):
+    # Responds to keypresses.
+    if event.key == pygame.K_RIGHT:
+                ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    
+def check_keyup_events(event, ship):
+    # Responds to key releases.
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
     '''Responds to keypresses and mouse events.'''
     for event in pygame.event.get():
@@ -8,18 +22,23 @@ def check_events(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
+        elif.event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+        
+            # if event.key == pygame.K_RIGHT:
+            #     ship.moving_right = True
+            # elif event.key == pygame.K_LEFT:
+            #     ship.moving_left = True
                 # Move ship to the right
                 # ship.rect.centerx += 1
 
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+        # elif event.type == pygame.KEYUP:
+        #     if event.key == pygame.K_RIGHT:
+        #         ship.moving_right = False
+        #     elif event.key == pygame.K_LEFT:
+        #         ship.moving_left = False
 
             # if event.key == pygame.K_LEFT:
             #     ship.rect.centerx -= 1
