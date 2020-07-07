ec039d033132605c71631501da3a621456b2a6b6
HHT <harashtatandjung@gmail.com>
2020-07-07 18:12:16 +0700

    change background color

--- PCC/Part_2_Projects/project_1.py
+++ PCC/Part_2_Projects/project_1.py
@@ -18,8 +18,11 @@ import sys
 def run_game():
     # Initialize game and create a screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    screen = pygame.display.set_mode((1000,600)) # originally 1200, 800
     pygame.display.set_caption("Alien Invasion")
+
+    # Set the background color.
+    bg_color = (230, 230, 230)
 
     # Start the main loop for the game.
     while True:
@@ -28,6 +31,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # Redraw the screen during each pass through the loop.
+        screen.fill(bg_color)
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
