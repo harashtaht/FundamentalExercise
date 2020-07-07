c215b5d1c9f5b70e1425a0d99cfa538440951984
HHT <harashtatandjung@gmail.com>
2020-07-07 19:49:25 +0700

    ship control added

--- PCC/Part_2_Projects/alien_invasions.py
+++ PCC/Part_2_Projects/alien_invasions.py
@@ -17,6 +17,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     # Initialize game and create a screen object.
@@ -29,16 +30,12 @@ def run_game():
     # Make a ship.
     ship = Ship(screen)
 
-    # Set the background color.
-    # bg_color = (230, 230, 230)
-
     # Start the main loop for the game.
     while True:
 
-        # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
