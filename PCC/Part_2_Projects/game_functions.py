696c57bc38c6e640f5ef53f4acb5704ea2e1f61c
HHT <harashtatandjung@gmail.com>
2020-07-08 11:33:00 +0700

    left button config

--- PCC/Part_2_Projects/game_functions.py
+++ PCC/Part_2_Projects/game_functions.py
@@ -2,16 +2,37 @@ import sys
 import pygame
 from bullet import Bullet
 
-def check_keydown_events(event, ai_settings, screen, ship, bullets):
+# def check_keydown_events(event, ai_settings, screen, ship, bullets):
+#     # Responds to keypresses.
+#     if event.key == pygame.K_RIGHT:
+#         ship.moving_right = True
+#     elif event.key == pygame.K_LEFT:
+#         ship.moving_left = True
+#     elif event.key == pygame.K_SPACE:
+#         # Create a new bullet and add it to the bullets group.
+#         new_bullet = Bullet(ai_settings, screen, ship)
+#         bullets.add(new_bullet)
+
+
+# def check_events(ai_settings, screen, ship, bullets):
+#     '''Responds to keypresses and mouse events.'''
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             sys.exit()
+
+#         elif event.type == pygame.KEYDOWN:
+#             check_keydown_events(event, ai_settings, screen, ship, bullets)
+#         elif event.type == pygame.KEYUP:
+#             check_keyup_events(event, ship)
+
+# ---------------- #
+# Initial function
+def check_keydown_events(event, ship):
     # Responds to keypresses.
     if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
+        ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
-    elif event.key == pygame.K_SPACE:
-        # Create a new bullet and add it to the bullets group.
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
     
 def check_keyup_events(event, ship):
     # Responds to key releases.
@@ -20,14 +41,13 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ship):
     '''Responds to keypresses and mouse events.'''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ai_settings, screen, ship, bullets)
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
