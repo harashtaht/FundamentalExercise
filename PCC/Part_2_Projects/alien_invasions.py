14d1e653ab07051bbf5df2c1576036969b45cdd4
HHT <harashtatandjung@gmail.com>
2020-07-08 09:01:00 +0700

    fire

--- PCC/Part_2_Projects/alien_invasions.py
+++ PCC/Part_2_Projects/alien_invasions.py
@@ -15,6 +15,7 @@ If the player loses three ships, the game ends.
 
 import sys
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -30,12 +31,16 @@ def run_game():
     # Make a ship.
     ship = Ship(ai_settings, screen)
 
+    # Make a group to store bullets in.
+    bullets = Group()
+
     # Start the main loop for the game.
     while True:
 
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
