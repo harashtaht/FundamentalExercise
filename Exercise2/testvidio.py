b3e506f1598b0e80dbd92252f7cf3e30205bbf8d
HHT <harashtatandjung@gmail.com>
2020-09-11 22:29:41 +0700

    q2 init

---
+++ Exercise2/testvidio.py
@@ -0,0 +1,23 @@
+### Number 1 ###
+
+# Example
+# displayMulti("Jakarta", 2) --> JakJak
+# displayMulti("Jakarta", 3) --> JakJakJak
+# displayMulti("BBM", 2) --> BBMBBM
+# displayMulti("Or", 3) --> rOrOrO
+
+# def displayMulti(string, num):
+#     output = ""
+#     if len(string) >= 3:
+#         output += (string[:3]*num)
+#     else:
+#         output += (string[::-1]*num)
+#     print(output)
+
+# displayMulti("Jakarta", 2)
+# displayMulti("Jakarta", 3)
+# displayMulti("BBM", 2)
+# displayMulti("Or", 3)
+
+
+### Number 1 ###
