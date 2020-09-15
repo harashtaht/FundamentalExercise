56dc21c9762bc288b6a700b6c01c79e9de7a3f01
HHT <harashtatandjung@gmail.com>
2020-09-15 08:09:12 +0700

    Class for displaying, separation not done

--- Exercise2/testvidio.py
+++ Exercise2/testvidio.py
@@ -34,13 +34,53 @@
 # Jean Summers – 08001001001 : insert success
 # '''
 
-# inp = "Charlie,Zoe,08123123123;Andre,Xavier,08111222333;Charlie,Xyz,08123123123;Jean,Summers,08001001001"
+inp = "Charlie,Zoe,08123123123;Andre,Xavier,08111222333;Charlie,Xyz,08123123123;Jean,Summers,08001001001"
+
+addressbook = []
+for i in inp.split(sep=";"):
+    # _ = []
+    print(i)
+    # _.append(i)
+    # addressbook.append(_)
+    addressbook.append(i)
+
+# print(addressbook)
+
+for i in addressbook:
+    # print(type(i.split(sep=",")))
+    print(i.split(sep=","))
 
 
-# for i in inp.split(sep=","):
+# print(addressbook[0]) #['Charlie,Zoe,08123123123']
+# print(len(addressbook)) #4
+
+# for i in addressbook[0].split(sep=","):
 #     print(i)
 
-# print(type(inp))
+# for i in addressbook[0]:
+#     # print(type(i))
+#     print(i)
 
-a = "ak"
-print(a)
+# Cari threshold untuk check nomor sudah di input (duplikat)
+
+
+# Class untuk display Log
+# insert success/duplicate phone number not done
+
+list = ["Charlie,Zoe,08123123123"]
+
+class Address():
+
+    def __init__(self, first_name, last_name, number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.number = number
+
+    def show_input(self):
+        data_add = self.first_name + " " + self.last_name + " - " + self.number
+        print(data_add)
+
+    def check_duplicate(self):
+        return None
+
+# Address('Charlie', 'Zoe', '08123123123').show_input()
