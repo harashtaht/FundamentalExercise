7abdaaa552c2748b26f33c306bb063b9239ac51c
HHT <harashtatandjung@gmail.com>
2020-10-29 19:51:53 +0700

    division

--- Hackerrank/1.intro.py
+++ Hackerrank/1.intro.py
@@ -3,14 +3,25 @@
 
 # n = int(input())
 
-if __name__ == '__main__':
-    n = int(input().strip())
+# if __name__ == '__main__':
+#     n = int(input().strip())
 
-if n%2 != 0:
-    print("Weird")
-elif (n%2 == 0) and (2<=n<=5):
-    print("Not Weird")
-elif (n%2 == 0) and (6<=n<=20):
-    print("Weird")
-elif (n%2 == 0) and (n>20):
-    print("Not Weird")
+# if n%2 != 0:
+#     print("Weird")
+# elif (n%2 == 0) and (2<=n<=5):
+#     print("Not Weird")
+# elif (n%2 == 0) and (6<=n<=20):
+#     print("Weird")
+# elif (n%2 == 0) and (n>20):
+#     print("Not Weird")
+
+############################
+### Arithmetic Operators ###
+############################
+
+# a = int(input())
+# b = int(input())
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
