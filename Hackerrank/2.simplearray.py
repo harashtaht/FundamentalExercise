179bdb982f58def5083aed40f2c57d1f5320cb28
HHT <harashtatandjung@gmail.com>
2020-11-10 17:18:26 +0700

    array #2 init

--- Hackerrank/2.simplearray.py
+++ Hackerrank/2.simplearray.py
@@ -1,20 +1,22 @@
-## Part 1 ##
+# ## Part 1 ##
 
-b = input()
-b = int(b)
+# b = input()
+# b = int(b)
 
-if (b<0) or (b > 1000):
-    print("Constraints must be more than 0 or less than equal of 1000. Please enter another number.\n")
-    b = input()
-else:
-    a = input()
+# if (b<0) or (b > 1000):
+#     print("Constraints must be more than 0 or less than equal of 1000. Please enter another number.\n")
+#     b = input()
+# else:
+#     a = input()
 
-ls = []
-for i in a.split():
-    ls.append(int(i))
+# ls = []
+# for i in a.split():
+#     ls.append(int(i))
 
-results = 0
-for i in ls:
-    results += i
+# results = 0
+# for i in ls:
+#     results += i
 
-print(results)
+# print(results)
+
+# Part 2
