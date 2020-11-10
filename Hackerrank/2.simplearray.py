268d465b46e04a323abe4406aaa88fbc221ea365
HHT <harashtatandjung@gmail.com>
2020-11-10 18:07:42 +0700

    part2 mod2

--- Hackerrank/2.simplearray.py
+++ Hackerrank/2.simplearray.py
@@ -20,3 +20,36 @@
 # print(results)
 
 # Part 2
+
+a = "17 28 30"
+b = "99 16 8"
+
+def appendList(x):
+    ls = []
+    for i in x.split():
+        ls.append(int(i))
+    return ls
+
+a = appendList(a)
+b = appendList(b)
+
+def compareTriplets(a,b):
+    num_a = 0
+    num_b = 0
+    for i in range(len(a)):
+        if a[i] > b[i]:
+            num_a += 1
+        elif b[i] > a[i]:
+            num_b += 1
+        else:
+            num_a += 0
+            num_b += 0
+    ls = []
+    ls.append(num_a)
+    ls.append(num_b)
+    return ls
+
+results = compareTriplets(a,b)
+print(results)
+
+# Need Constraints
