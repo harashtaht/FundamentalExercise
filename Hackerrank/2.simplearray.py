fb07d9a5ebdcc6c5c6b521009102486fc5509de6
HHT <harashtatandjung@gmail.com>
2020-11-10 16:45:40 +0700

    nov1

--- Hackerrank/2.simplearray.py
+++ Hackerrank/2.simplearray.py
@@ -1,3 +1,17 @@
 ## Part 1 ##
 
-a = int(input())
+# a = (input())
+a = '2 4 1 20 18'
+
+# print(a)
+# print(type(a))
+ls = []
+for i in a.split():
+    ls.append(int(i))
+
+results = 0
+for i in ls:
+    results += i
+    print(results)
+
+print(results)
