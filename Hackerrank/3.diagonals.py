eea4ee3873080db900da9712b007c9d8e90759a5
HHT <harashtatandjung@gmail.com>
2020-11-20 13:40:39 +0700

    part 2 done

--- Hackerrank/3.diagonals.py
+++ Hackerrank/3.diagonals.py
@@ -6,24 +6,40 @@
 # 4 5 6
 # 10 8 -12
 
-n = int(input())
+# n = int(input())
 
-toCount = [list(int(x) for x in input().split()) for i in range(n)]
+# toCount = [list(int(x) for x in input().split()) for i in range(n)]
 
-def diagonalDifference(list):
-    d1 = sum(list[x][x] for x in range(n))
-    d2 = sum(list[x][n-1-x] for x in range(n))
-    result = abs(d1-d2)
-    return result
+# def diagonalDifference(list):
+#     d1 = sum(list[x][x] for x in range(n))
+#     d2 = sum(list[x][n-1-x] for x in range(n))
+#     result = abs(d1-d2)
+#     return result
 
-print(diagonalDifference(toCount))
+# print(diagonalDifference(toCount))
 
 # Part 2: ratio of integers
 
-# n = int(input())
+n = int(input())
 
-# arr = input()
-# arr1 = [list(int(x) for x in arr.split())]
-# # list = []
-# # arr1 = [list.append(i) for i in arr.split()]
-# print(arr1)
+arr = list(map(int, input().split()))
+
+def plusMinus(list):
+    positive = 0
+    negative = 0
+    zeros = 0
+    for i in range(len(list)):
+        if list[i] > 0:
+            positive += 1
+        elif list[i] <0:
+            negative += 1
+        else:
+            zeros += 1
+    pos = positive/len(list)
+    neg = negative/len(list)
+    zer = zeros/len(list)
+    print(f'{pos:.6f}')
+    print(f'{neg:.6f}')
+    print(f'{zer:.6f}')
+
+plusMinus(arr)
