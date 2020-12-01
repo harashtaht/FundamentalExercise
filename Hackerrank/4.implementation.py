6c2bdb11218e03f2828fd54cdd6b3559998f7e4e
HHT <harashtatandjung@gmail.com>
2020-12-01 15:52:13 +0700

    answer1

---
+++ Hackerrank/4.implementation.py
@@ -0,0 +1,34 @@
+# Part 1: catAndMouse
+
+#in: 2 5 4 out: cat B
+#in: 1 2 3 out: cat B
+#in: 1 3 2 out: mouse C
+
+def catAndMouse(str):
+    nums = [int(x) for x in str.split()]
+    numA = nums[0]
+    numB = nums[1]
+    numC = nums[-1]
+
+    if (numC>numA) and (numC>numB):
+        if abs(numC-numA) < abs(numC-numB):
+            print("cat A")
+        elif abs(numC-numA) > abs(numC-numB):
+            print("cat B")
+        elif abs(numC-numA) == abs(numC-numB):
+            print("mouse C")
+    elif (numC<numA) or (numC<numB):
+        if abs(numC-numA) < abs(numC-numB):
+            print("cat A")
+        elif abs(numC-numA) > abs(numC-numB):
+            print("cat B")
+        elif abs(numC-numA) == abs(numC-numB):
+            print("mouse C")    
+
+q = int(input())
+ls = []
+for i in range(q):
+    ls.append(input())
+
+for i in ls:
+    catAndMouse(i)
